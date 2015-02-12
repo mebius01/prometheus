c5901d60382647c0f66e7ca5609a008bae956704
mebius01 <mebius01@mail.ru>
2015-02-12 17:27:47 +0200

    end

--- job-4-4.py
+++ job-4-4.py
@@ -19,3 +19,23 @@
  Результат: 7
  Пояснення: номери 222222, 222231, 222240, 222303, 222312, 222321, 222330
  """
+import sys, math
+s1=sys.argv[1]; s1=int(s1); s2=sys.argv[2]; s2=int(s2); 
+l=[]; ll=[]
+
+while s1 < s2+1:
+	l.insert(-1,str(s1))
+	s1+=1
+
+for i in l:
+	if len(i) < 6:
+		while len(i) < 6:
+			i='0'+i; 
+	ll.insert(-1, i); 
+del l[0:]
+
+for i in ll:
+	if (int(i[0])+int(i[1])+int(i[2])) == (int(i[3])+int(i[4])+int(i[5])):
+		l.append(i)
+
+print len(l)
