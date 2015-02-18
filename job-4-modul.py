986d7f15f46bdced826e95cfebc7f4e605726aa4
mebius <mebius@debian>
2015-02-18 23:40:25 +0200

    add key-v

--- job-4-modul.py
+++ job-4-modul.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-s="qwERqTYqwR lblbDUDT KJVBHljbb KJBH lkjbnj"
+s="Hot sUn BEATIng dOWN bURNINg mY FEet JuSt WalKIng arOUnD HOt suN mAkiNG me SWeat"
 
 z=0
 l=[]
-#~ while z < len(s):
-	#~ l.append(s[z:z+5])
-	#~ z+=5
-#~ for i in l:
-	#~ if len(i) < 5:
-		#~ del l[l.index(i)]
+
+
 
 for i in s:
 	if i == " ":
 		s=s.replace(i, '')
 	elif i.islower() == True:
 		s=s.replace(i, "a")
-	elif i.isupper() == True:
-		s=s.replace(i, "b")
 
 for i in s:
 	if i.isupper() == True:
 		s=s.replace(i, "b")
 print s
+
+while z < len(s):
+	l.append(s[z:z+5])
+	z+=5
+	for i in l:
+		if len(i) < 5:
+			del l[l.index(i)]
+
+
+l_key={'a':"aaaaa", 'b':"aabbb", 'c':"aaabb", 'd':"aaaab", 'e':"abbbb",
+ 'f':"bbbbb--", 'g':"bbbba", 'h':"bbbab", 'i':"bbabb", 'j':"babbb", 
+ 'k':"abbba", 'l':"bbbaa", 'm':"bbaab", 'n':"baabb", 'o':"aabba", 'p':"abbab", 'q':"", 
+ 'r':"babab", 's':"ababb", 't':"babba", 'u':"abbaa", 'v':"", 'w':"baaab", 'x':"", 'y':"aabab", 'z':""}
+
