c5074b01a2f33596728f8acabe2bdb81219a8fb9
mebius01 <mebius01@mail.ru>
2015-02-19 11:04:27 +0200

    ok

--- job-4-modul.py
+++ job-4-modul.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-s="Hot sUn BEATIng dOWN bURNINg mY FEet JuSt WalKIng arOUnD HOt suN mAkiNG me SWeat"
+s="wELcO MeToT heHOt ELcaL iFORN IaSUc HALoV eLYPL aCEsu cHaLO vely"
 
 z=0
 l=[]
@@ -26,7 +26,30 @@ while z < len(s):
 		if len(i) < 5:
 			del l[l.index(i)]
 
+print l
 
-l_key={'a':"aaaaa", 'b':"aabbb", 'c':"aaabb", 'd':"aaaab", 'e':"abbbb", 'f':"", 'g':"", 'h':"", 'i':"bbabb", 'j':"", 'k':"abbba", 'l':"bbbaa", 
-'m':"", 'n':"", 'o':"", 'p':"", 'q':"", 'r':"", 's':"", 't':"", 'u':"", 'v':"", 'w':"", 'x':"", 'y':"", 'z':""]
+dic={"aaaaa":'a', "aaaab":'b', "aaabb":'c', 
+	"aabbb":'d', "abbbb":'e', "bbbbb":'f', 
+	"bbbba":'g', "bbbaa":'h', "bbabb":'i', 
+	"babbb":'j', "abbba":'k', "bbbaa":'l', 
+	"bbaab":'m', "baabb":'n', "aabba":'o', 
+	"abbab":'p', "bbaba":'q', "babab":'r', 
+	"ababb":'s', "babba":'t', "abbaa":'u', 
+	"bbaaa":'v', "baaab":'w', "aaaba":'x', 
+	"aabab":'y', "ababa":'z'}
 
+for i in dic.keys():
+	for a in l:
+		if i == a:
+			print dic.get(i)
+"""
+dic={'a':"aaaaa", 'b':"aaaab", 'c':"aaabb", 
+	'd':"aabbb", 'e':"abbbb", 'f':"bbbbb", 
+	'g':"bbbba", 'h':"bbbaa", 'i':"bbabb", 
+	'j':"babbb", 'k':"abbba", 'l':"bbbaa", 
+	'm':"bbaab", 'n':"baabb", 'o':"aabba", 
+	'p':"abbab", 'q':"bbaba", 'r':"babab", 
+	's':"ababb", 't':"babba", 'u':"abbaa", 
+	'v':"bbaaa", 'w':"baaab", 'x':"aaaba", 
+	'y':"aabab", 'z':"ababa"}
+"""
