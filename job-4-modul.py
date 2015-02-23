96a6d2a0aaa17176ccb29ae6a971d990b61326fd
mebius01 <mebius01@mail.ru>
2015-02-23 15:59:58 +0200

    a

--- job-4-modul.py
+++ job-4-modul.py
@@ -1,36 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-s="wELcO MeToT heHOt ELcaL iFORN IaSUc HALoV eLYPL aCEsu cHaLO vely"
-
+import sys
+string_input=sys.argv[1]
+string_output=''
 z=0
 l=[]
-
-
-
-for i in s:
-	if i == " ":
-		s=s.replace(i, '')
-	elif i.islower() == True:
-		s=s.replace(i, "a")
-
-for i in s:
-	if i.isupper() == True:
-		s=s.replace(i, "b")
-print s
-
-while z < len(s):
-	l.append(s[z:z+5])
-	z+=5
-	for i in l:
-		if len(i) < 5:
-			del l[l.index(i)]
-
-print l
-
 dic={"aaaaa":'a', "aaaab":'b', "aaabb":'c', 
 	"aabbb":'d', "abbbb":'e', "bbbbb":'f', 
-	"bbbba":'g', "bbbaa":'h', "bbabb":'i', 
+	"bbbba":'g', "bbbab":'h', "bbabb":'i', 
 	"babbb":'j', "abbba":'k', "bbbaa":'l', 
 	"bbaab":'m', "baabb":'n', "aabba":'o', 
 	"abbab":'p', "bbaba":'q', "babab":'r', 
@@ -38,14 +16,34 @@ dic={"aaaaa":'a', "aaaab":'b', "aaabb":'c',
 	"bbaaa":'v', "baaab":'w', "aaaba":'x', 
 	"aabab":'y', "ababa":'z'}
 
-for i in dic.keys():
-	for a in l:
+for i in string_input:
+	if i == " ":
+		string_input=string_input.replace(i, '')
+	elif i.islower() == True:
+		string_input=string_input.replace(i, "a")
+
+for i in string_input:
+	if i.isupper() == True:
+		string_input=string_input.replace(i, "b")
+
+while z < len(string_input):
+	l.append(string_input[z:z+5])
+	z+=5
+	for i in l:
+		if len(i) < 5:
+			del l[l.index(i)]
+
+for a in l:
+	for i in dic.keys():
 		if i == a:
-			print dic.get(i)
+			string_output=string_output + dic.get(i)
+
+print(string_output)
+
 """
 dic={'a':"aaaaa", 'b':"aaaab", 'c':"aaabb", 
 	'd':"aabbb", 'e':"abbbb", 'f':"bbbbb", 
-	'g':"bbbba", 'h':"bbbaa", 'i':"bbabb", 
+	'g':"bbbba", 'h':"bbbab", 'i':"bbabb", 
 	'j':"babbb", 'k':"abbba", 'l':"bbbaa", 
 	'm':"bbaab", 'n':"baabb", 'o':"aabba", 
 	'p':"abbab", 'q':"bbaba", 'r':"babab", 
--- job-5-1.py
+++ job-5-1.py
@@ -17,16 +17,16 @@
  Виклик функції: clean_list([1, 2, 1, 1, 3, 4, 5, 4, 6, '2', 7, 8, 9, 0, 1, 2, 3, 4, 5])
  Повертає: [1, 2, 3, 4, 5, 6, '2', 7, 8, 9, 0]
  """
-import sys
-list_to_cleansys=sys.argv[1:]
-print list_to_cleansys
-def clean_list(lis):
-	z=0
-	while len(lis) > z:
-		for i in lis:
-			if (i == lis[-z]) == True:
-				del lis[-z]
-		z+=1
+input_list = [[32, 32.1, 32.0, -123]]
+def clean_list(l):
+	output_list = []
+	for i in l:
+		typ=type(i)
+		if i not in output_list:
+			output_list.append(i)
+		if i in output_list:
+			if type(i) != type(output_list[output_list.index(i)]):
+				output_list.append(typ(i))
+	return(output_list)
 
-clean_list(list_to_cleansys)
-print list_to_cleansys
+print clean_list(input_list)
