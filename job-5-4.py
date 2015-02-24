1930fe3d3914c8caa7d49c480d57eede21d450f0
mebius01 <mebius01@mail.ru>
2015-02-24 16:44:26 +0200

    .

--- job-5-4.py
+++ job-5-4.py
@@ -2,21 +2,44 @@
 # -*- coding: utf-8 -*-
 """
 Розробити функцію file_search(folder, filename), 
- яка приймає 2 аргументи -- список folder та рядок filename, 
- та повертає рядок -- повний шлях до файлу або папки filename в структурі folder.
+яка приймає 2 аргументи -- список folder та рядок filename, 
+та повертає рядок -- повний шлях до файлу або папки filename в структурі folder.
 
 Файлова структура folder задається наступним чином: 
 
-Список -- це папка з файлами, його 0-й елемент містить назву папки, а всі інші можуть представляти або файли в цій папці (1 файл = 1 рядок-елемент списку), або вкладені папки, які так само представляються списками. Як і в файловій системі вашого комп'ютера, шлях до файлу складається з імен всіх папок, в яких він міститься, в порядку вкладеності (починаючи з зовнішньої і до папки, в якій безпосередньо знаходиться файл), розділених "/". 
+Список -- це папка з файлами, його 0-й елемент містить назву папки, 
+а всі інші можуть представляти або файли в цій папці (1 файл = 1 рядок-елемент списку), 
+або вкладені папки, які так само представляються списками. Як і в файловій системі вашого комп'ютера, 
+шлях до файлу складається з імен всіх папок, в яких він міститься, 
+в порядку вкладеності (починаючи з зовнішньої і до папки, в якій безпосередньо знаходиться файл), 
+розділених "/". 
 
 Вважати, що імена всіх файлів є унікальними. Повернути логічне значення False, якщо файл не знайдено.
 
-Наприклад
- Виклик функції: file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt')
- Повертає: 'C:/ideas.txt'
- Виклик функції: file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me')
- Повертає: False
- Виклик функції: file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py')
- Повертає: '/home/user2/desktop/new folder/hereiam.py'
- """
- 
+Виклик функції: file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt')
+Повертає: 'C:/ideas.txt'
+
+Виклик функції: file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me')
+Повертає: False
+
+Виклик функції: file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py')
+Повертає: '/home/user2/desktop/new folder/hereiam.py'
+"""
+
+def file_search(a, b):
+	s=a[0]+'/'
+	if b in a:
+		for i in a:
+			if i == b:
+				s=s+i
+	print s
+	#~ if type(a) == list:
+		#~ for i in a:
+			#~ print i == b
+			#~ if type(i) == list:
+				#~ file_search(i, b)
+			
+
+file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt')
+file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me')
+file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py')
