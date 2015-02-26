27fee1139479b50c822d3cde05c66e5552b57dcd
mebius01 <mebius01@mail.ru>
2015-02-26 17:38:23 +0200

    it is a big ass, fucking recursion

--- job-5-4.py
+++ job-5-4.py
@@ -26,29 +26,50 @@
 Повертає: '/home/user2/desktop/new folder/hereiam.py'
 """
 #~ 
-def file_search(a, b):
-	s=a[0]+"/"
-	if b in a:
-		return(s+b)
-	if len(a) > 1:
-		for i in a:
-			if isinstance(i, list):
-				recursion = file_search(i, b)
-				if recursion != None:
-					s = s + recursion
-					return(s)
-				
+#~ def file_search(lis, fil):
+	#~ pat=lis[0]+'/'
+	#~ if fil in lis:
+		#~ return pat+fil
+	#~ elif fil not in lis:
+		#~ pat=lis[0]+'/'
+		#~ for i in lis:
+			#~ if isinstance(i, list):
+				#~ recursion=file_search(i, fil)
+				#~ if recursion != None:
+					#~ pat=pat+recursion
+					#~ return(s)
+
+"""
+Напишу Вам алгоритм моей программы, а Вы уж сами его адаптируйте под свои нужды.
+
+    Сразу записываю в переменную path=folder[0]+'/'
+
+    Проверяю, есть ли нужный файл в папке ( ну чтобы лишний раз не гонять), и если да, 
+    то return path+filename
+
+    Если файла в папке нет, опять записываю в переменную path=folder[0]+'/'. 
+    Затем пробегаю по всем элементам в папке. Если какой-то из элементов тоже папка, 
+    то результат рекурсивного запроса записываю в переменную res=file_search(element, filename) 
+    и сразу же проверяю не пустой ли запрос. Если запрос не пустой, 
+    то к пути добавляю результат запроса и возвращаю путьpath=path+res; return path
+
+Все это навеяно изучением вопросов по этой теме здесь в обсуждениях. Кстати, моя прога долго не хотела работать, пока я не добавил return path :)
+
+Надеюсь помог. Удачи!
+http://edx.prometheus.org.ua/courses/KPI/Programming101/2015_T1/discussion/forum/89da7e34a21749489846de4da3a7447f/threads/54ec466ff652d84b690000a9
+"""
+ 
 #~ def file_search(a, b):
 	#~ s=a[0]+"/"
 	#~ for i in a:
 		#~ if isinstance(i, str):
 			#~ if i == b:
-				#~ return(s+b)
+				#~ return(i+b)
 		#~ elif isinstance(i, list)== True:
 			#~ if len(i) > 1:
 				#~ recursion=file_search(i, b)
-				#~ s = s + recursion
 				#~ if s != None:
+					#~ s = s + recursion
 					#~ return(s)
 #~ def file_search(folder, filename):
     #~ if isinstance(folder, list):
@@ -69,8 +90,17 @@ print file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt')
 print file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me')
 print file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py')
 
-	#~ elif b not in a:
-		#~ return (a[0]+"/"+file_search(a, b))
+#~ def file_search(a, b):
+	#~ s=a[0]+"/"
+	#~ if b in a:
+		#~ return(s+b)
+	#~ if len(a) > 1:
+		#~ for i in a:
+			#~ if isinstance(i, list):
+				#~ recursion = file_search(i, b)
+				#~ if recursion != None:
+					#~ s = s + recursion
+					#~ return(s)
 
 
 #~ s=a[0]+'/'
