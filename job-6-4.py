91bc510c2f180ae50bdf9bfc249317cb210db6b6
mebius01 <mebius01@mail.ru>
2015-02-28 10:54:17 +0200

    .

--- job-6-4.py
+++ job-6-4.py
@@ -23,13 +23,23 @@
 
 """
 def find_most_frequent(s):
-	l=''
+	l=''; l2=[]
 	for i in s:
 		if i.isalnum() != True:
 			l=l+" "
 		else: l=l+i
 	l=l.split()
-	return l
+	for i in l:
+		if l.count(i) > 1:
+			l2.append(i) 
+	l2 = dict([(item, None) for item in l2]).keys() #http://softwaremaniacs.org/blog/2006/10/03/python-list-duplicates/
+	l2.sort()
+	return l2
+
+print find_most_frequent('Mike-Paul mike'), ['mike']
+print find_most_frequent("Load up on guns; bring your friends It's fun to lose and to pretend She's over-bored and self-assured Oh no, I know a dirty word Hello,hello,hello,how low Hello,hello,hello,howlow Hello,hello,hello,how low Hello,hello,hello"), ['hello']
+print find_most_frequent("And I forget just why I taste; Oh yeah, I guess it makes me smile; I found it hard; it's hard to find; Oh well, whatever, never mind"), ['i']
+print find_most_frequent('This is the front page of the Simple English Wikipedia. Wikipedias are places where people work together to write encyclopedias in different languages. We use Simple English words and grammar here. The Simple English Wikipedia is for everyone! That includes children and adults who are learning English. There are 120,571 articles on the Simple English Wikipedia. All of the pages are free to use. They have all been published under both the Creative Commons Attribution/Share-Alike License 3.0 and GNU Free Documentation License. You can help here! You may change these pages and make new pages. Read help pages and other good pages to learn how to write pages here. If you need help, you may ask questions at Simple talk.'), ['pages', 'the']
 
 
 print find_most_frequent('Hello,Hello, my dear!')
